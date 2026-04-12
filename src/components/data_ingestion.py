41b9b4fccb129e1ac42a022243d29e5fe977260b
Bhanu Prakash <bhanuprakashkalla.1995@gmail.com@example.com>
2026-04-12 23:31:32 +0530

    model_trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,11 @@ from dataclasses import dataclass
 from src.exception import CustomException
 from src.logger import logging
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 # ==============================
 # Config Class
@@ -68,5 +73,11 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_path, test_path = obj.initiate_data_ingestion()
-    print("Train path:", train_path)
-    print("Test path:", test_path)
+    
+
+    data_transformation=DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
